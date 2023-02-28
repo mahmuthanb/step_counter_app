d964478a338e99879de956427654d0fb5527b419
Mahmuthan <mahmuthanb@gmail.com>
2023-02-28 20:45:28 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/mahmuthan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mahmuthan.step_counter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:step_counter_app/app/app.dart';
+import 'package:step_counter_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
